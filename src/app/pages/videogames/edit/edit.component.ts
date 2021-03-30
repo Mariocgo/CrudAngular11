eba475dcf767d8a1b916ac4e456e82b7d63f5679
mariocgo <e17081448@itmerida.edu.mx>
2021-03-30 16:19:42 -0600

    Con todo 2

--- src/app/pages/videogames/edit/edit.component.ts
+++ src/app/pages/videogames/edit/edit.component.ts
@@ -1,12 +1,8 @@
-import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Games } from 'src/app/shared/models/games.interface';
 
 @Component({
   selector: 'app-edit',
-  templateUrl: `<app-games-form></app-games-form>`,
+  template: `<app-games-form></app-games-form>`,
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent {
